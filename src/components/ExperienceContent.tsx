b29a06710d2fc8005481255690d4adc358539b51
T095T <pdeshpande540@gmail.com>
2025-11-29 23:20:02 +0530

    experience content section added

---
+++ src/components/ExperienceContent.tsx
@@ -0,0 +1,14 @@
+interface ExperienceContentProps{
+ children:React.ReactNode
+ className?:string   
+}
+
+export default function ExperienceContent(){
+
+    
+    return(
+        <div className="space-y-4 dark:text-white/70 text-black/70 pb-4">
+
+        </div>
+    )
+}
